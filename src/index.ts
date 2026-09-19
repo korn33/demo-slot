4f4be2371c0ac88d5ec6ac935d7d9126ea1900f8
korn33 <48405854+korn33@users.noreply.github.com>
2026-09-19 17:38:42 +0300

    added pixi js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-console.log('demo-slot');
-console.log('demo-slot - 2');
-console.log('demo-slot - 3');
+import {Application, Graphics} from "pixi.js";
+
+async function main() {
+    const app = new Application();
+    await app.init({background: '#1099bb', resizeTo: window});
+    document.body.appendChild(app.canvas);
+
+    const square = new Graphics().rect(-50, -50, 100, 100).fill(0xffffff);
+    square.position.set(app.screen.width / 2, app.screen.height / 2);
+    app.stage.addChild(square);
+
+    app.ticker.add((ticker) => {
+        square.rotation += 0.02 * ticker.deltaTime;
+    });
+}
+
+main();
